aa6057f78ea60290587ef2046a91c997885d3a6c
Alex Lalejini <amlalejini@gmail.com>
2019-05-21 23:00:39 -0400

    Add setup and init skeletons to dol world.

--- source/DOLWorld.h
+++ source/DOLWorld.h
@@ -19,20 +19,82 @@
 EMP_BUILD_CONFIG( DOLWorldConfig,
   GROUP(MAIN, "Global Settings"),
   VALUE(SEED, int, -1, "Random number generator seed"),
+  VALUE(UPDATES, size_t, 1000, "Number of updates to run the experiment."),
+  VALUE(CPU_CYCLES_PER_UPDATE, size_t, 30, "Number of CPU cycles to distribute to each cell every update."),
+  VALUE(INIT_POP_SIZE, size_t, 1, "How many organisms should we seed the world with?"),
+  VALUE(MAX_POP_SIZE, size_t, 1000, "What is the maximum size of the population?"),
+
+  GROUP(DEME, "Deme Settings"),
+  VALUE(DEME_WIDTH, size_t, 5, "What is the maximum cell-width of a deme?"),
+  VALUE(DEME_HEIGHT, size_t, 5, "What is the maximum cell-height of a deme?"),
+
 );
 
 constexpr size_t TAG_WIDTH = 16;
 
+/// A 'deme' of cells.
 struct Deme {
 
 };
 
-struct DemeBioFilm {
+/// Struct that houses demes.
+struct DemeVat {
 
 };
 
 class DOLWorld : public emp::World<DigitalOrganism<TAG_WIDTH>> {
+public:
+  using org_t = DigitalOrganism<TAG_WIDTH>;
+
+protected:
+
+  // MAIN Configuration Settings
+  int SEED;
+  size_t UPDATES;
+  size_t CPU_CYCLES_PER_UPDATE;
+  size_t INIT_POP_SIZE;
+  size_t MAX_POP_SIZE;
+  // DEME Configuration Settings
+  size_t DEME_WIDTH;
+  size_t DEME_HEIGHT;
+
+  void InitConfigs(DOLWorldConfig & config);
+  void InitPop();
+
+public:
+
+  DOLWorld() {}
+  DOLWorld(emp::Random & r) : emp::World<org_t>(r) {}
+
+  ~DOLWorld() {}
+
+  size_t GetCPUCyclesPerUpdate() const { return CPU_CYCLES_PER_UPDATE; }
+  size_t GetDemeWidth() const { return DEME_WIDTH; };
+  size_t GetDemeHeight() const { return DEME_HEIGHT; };
+  size_t GetDemeCapacity() const { return DEME_WIDTH * DEME_HEIGHT; };
+
+  void Reset(DOLWorldConfig & config);
+  void Setup(DOLWorldConfig & config);
+
+  void RunStep();
+  void Run();
 
 };
+
+void DOLWorld::InitConfigs(DOLWorldConfig & config) {
+  // MAIN Configuration Settings
+  UPDATES = config.UPDATES();
+  CPU_CYCLES_PER_UPDATE = config.CPU_CYCLES_PER_UPDATE();
+  INIT_POP_SIZE = config.INIT_POP_SIZE();
+  MAX_POP_SIZE = config.MAX_POP_SIZE();
+  // DEME Configuration Settings
+  DEME_WIDTH = config.DEME_WIDTH();
+  DEME_HEIGHT = config.DEME_HEIGHT();
+}
+
+void DOLWorld::Setup(DOLWorldConfig & config) {
+  InitConfigs(config);
+  // todo - finish function
+}
 
 #endif
--- tests/unit_tests.cc
+++ tests/unit_tests.cc
@@ -2,6 +2,7 @@
 #include "catch.hpp"
 
 #include "TestStruct.h"
+#include "DOLWorld.h"
 
 TEST_CASE( "First test!", "[first]" ) {
     REQUIRE(true);
@@ -9,4 +10,26 @@ TEST_CASE( "First test!", "[first]" ) {
     s.a = 5;
     s.b = 10;
     REQUIRE(s.Add() == 15);
+}
+
+TEST_CASE( "DOLWorld Setup", "[world][setup]") {
+    // Create a configuration object
+    DOLWorldConfig config;
+    config.SEED(1);
+    config.UPDATES(50);
+    config.CPU_CYCLES_PER_UPDATE(3);
+    config.INIT_POP_SIZE(10);
+    config.MAX_POP_SIZE(200);
+    config.DEME_WIDTH(2);
+    config.DEME_HEIGHT(2);
+
+    emp::Random rnd(config.SEED());
+    DOLWorld world(rnd);
+    world.Setup(config);
+
+    REQUIRE(world.GetDemeWidth() == 2);
+    REQUIRE(world.GetDemeHeight() == 2);
+    REQUIRE(world.GetDemeCapacity() == 4);
+    REQUIRE(world.GetCPUCyclesPerUpdate() == 3);
+
 }
